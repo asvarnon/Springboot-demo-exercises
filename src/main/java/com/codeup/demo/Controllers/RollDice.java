cae4b9a2831a03a9cfadfd7a3277c8b76733ec28
Austin Varnon <austinvarnon@gmail.com>
2020-11-24 15:53:52 -0600

    UPDATE: working on number guess

---
+++ src/main/java/com/codeup/demo/Controllers/RollDice.java
@@ -0,0 +1,48 @@
+package com.codeup.demo.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Random;
+
+@Controller
+public class RollDice {
+
+    @GetMapping("/roll-dice")
+    public String showNumberOptions() {
+        return "/dice";
+    }
+
+    @PostMapping("/roll-dice/{num}")
+    public String showAnswer(@PathVariable int num, Model model) {
+        model.addAttribute("num", num);
+        if (num == rollDice(1, 6)) {
+
+        }
+        return "/dice";
+    }
+
+    public static int rollDice(int number, int nSides) {
+        int num = 0;
+        int roll = 0;
+        Random r = new Random();
+        if (nSides >= 3) {
+            for (int i = 0; i < number; i++) {
+                roll = r.nextInt(nSides) + 1;
+                System.out.println("Roll is:  " + roll);
+                num = num + roll;
+            }
+        } else {
+            System.out.println("Error num needs to be from 3");
+        }
+        return num;
+    }
+
+
+
+
+}
+
