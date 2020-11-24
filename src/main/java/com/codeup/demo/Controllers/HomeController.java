bdc32a511a261733a2b09fbb17cba98d3bc62144
Austin Varnon <austinvarnon@gmail.com>
2020-11-24 14:12:28 -0600

    used for each to make list

--- src/main/java/com/codeup/demo/Controllers/HomeController.java
+++ src/main/java/com/codeup/demo/Controllers/HomeController.java
@@ -1,8 +1,14 @@
 package com.codeup.demo.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -12,5 +18,27 @@ public class HomeController {
     public String landingPage(){
         return "This is the landing page!";
     }
+
+    @GetMapping("/home")
+    public String welcome(){
+        return "home";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm(Model model){
+        List<String> cohortNames = new ArrayList<>();
+        cohortNames.add("COBOL");
+        cohortNames.add("Draco");
+        model.addAttribute("cohortNames", cohortNames);
+
+        return "/join";
+    }
+
+    @PostMapping("/join")
+    public String postJoinForm(@RequestParam(name = "cohort") String cohort, Model model){
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "/join";
+    }
+
 
 }
