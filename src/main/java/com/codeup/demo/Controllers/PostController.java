4c6fcb421ef31d35ae90b73d87a4d05068fd2f66
Austin Varnon <austinvarnon@gmail.com>
2020-11-26 18:49:15 -0600

    FEAT: matched the index number displayed with input typed in query line.

--- src/main/java/com/codeup/demo/Controllers/PostController.java
+++ src/main/java/com/codeup/demo/Controllers/PostController.java
@@ -1,6 +1,7 @@
 package com.codeup.demo.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,14 +10,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class PostController {
 
-    @GetMapping("/posts") @ResponseBody
+    @GetMapping("/posts")
     public String posts(){
-        return "posts index page";
+        return "/posts/show";
     }
 
-    @GetMapping("/posts/{id}") @ResponseBody
-    public String post(@PathVariable int id){
-        return "post number: " + id;
+    @GetMapping("/posts/{id}")
+    public String post(@PathVariable int id, Model model){
+        model.addAttribute("postNum", id);
+
+        return "/posts/index";
     }
 
     @GetMapping("/posts/create") @ResponseBody
