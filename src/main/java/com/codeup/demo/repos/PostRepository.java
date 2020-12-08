401e04af8219fa0756cfc72ee257fd2e19d4c090
Austin Varnon <austinvarnon@gmail.com>
2020-12-08 14:34:07 -0600

    FEAT: implementing integration testing

--- src/main/java/com/codeup/demo/repos/PostRepository.java
+++ src/main/java/com/codeup/demo/repos/PostRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByTitleIsLike(String term);
-
+    Post findByTitle(String title);
 
 
 }
